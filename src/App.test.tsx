6f013e9ece5547f630fc353a7c5240c948d70e73
Petter <petter.f.andersson@gmail.com>
2023-08-19 17:23:10 +0200

    Add basic Grid and functionality to select movie to view details

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,53 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { useFetch } from 'usehooks-ts';
 import App from './App';
 
+jest.mock('usehooks-ts');
+
 describe('App', () => {
+  beforeEach(() => {
+    (useFetch as jest.Mock).mockReturnValue({
+      data: {
+        count: 1,
+        results: [
+          {
+            title: 'A New Hope',
+            episode_id: 4,
+            opening_crawl: 'It is a period of civil war. Rebel spaceships, striking from a hidden base',
+            director: 'George Lucas',
+            producer: 'Gary Kurtz, Rick McCallum',
+            release_date: '1977-05-25',
+          },
+          {
+            title: 'The Empire Strikes Back',
+            episode_id: 5,
+            opening_crawl: 'It is a dark time for the Rebellion. Although the Death Star has been destroyed',
+            director: 'Irvin Kershner',
+            producer: 'Gary Kurtz, Rick McCallum',
+            release_date: '1980-05-17',
+          },
+        ],
+      },
+      error: undefined,
+    });
+  });
+
   it('should render a list of movies', () => {
     render(<App />);
 
     expect(screen.getByText(/A New Hope/i)).toBeInTheDocument();
-    expect(screen.getAllByRole('listitem')).toHaveLength(6);
+    expect(screen.getAllByRole('button')).toHaveLength(2);
   });
 
-  it('should render movie details when a movie is selected', () => {
+  it('should render movie details when a movie is selected', async () => {
+    const user = userEvent.setup();
     render(<App />);
+
+    const movies = screen.getAllByRole('button');
+    const firstMovie = movies[0];
+
+    await user.click(firstMovie);
 
     expect(
       screen.getByText(/It is a period of civil war. Rebel spaceships, striking from a hidden base/i)
@@ -27,12 +63,12 @@ describe('App', () => {
   it('should sort movies based on year (ascending) by default', () => {
     render(<App />);
 
-    const listItems = screen.getAllByRole('listitem');
-    const firstMovie = listItems[0];
-    const lastMovie = listItems.at(-1);
+    const movies = screen.getAllByRole('button');
+    const firstMovie = movies[0];
+    const lastMovie = movies.at(-1);
 
     expect(firstMovie).toHaveTextContent(/A New Hope/i);
-    expect(lastMovie).toHaveTextContent(/Revenge of the Sith/i);
+    expect(lastMovie).toHaveTextContent(/The Empire Strikes Back/i);
   });
 
   it('should sort movies based on year (descending)', async () => {
@@ -42,9 +78,9 @@ describe('App', () => {
     const sortButton = screen.getByText(/Sort by year (descending)/i);
     await user.click(sortButton);
 
-    const listItems = screen.getAllByRole('listitem');
-    const firstMovie = listItems[0];
-    const lastMovie = listItems.at(-1);
+    const movies = screen.getAllByRole('button');
+    const firstMovie = movies[0];
+    const lastMovie = movies.at(-1);
 
     expect(firstMovie).toHaveTextContent(/Revenge of the Sith/i);
     expect(lastMovie).toHaveTextContent(/A New Hope/i);
@@ -56,9 +92,9 @@ describe('App', () => {
     const sortButton = screen.getByText(/Sort by episode (ascending)/i);
     sortButton.click();
 
-    const listItems = screen.getAllByRole('listitem');
-    const firstMovie = listItems[0];
-    const lastMovie = listItems.at(-1);
+    const movies = screen.getAllByRole('button');
+    const firstMovie = movies[0];
+    const lastMovie = movies.at(-1);
 
     expect(firstMovie).toHaveTextContent(/The Phantom Menace/i);
     expect(lastMovie).toHaveTextContent(/Return of the Jedi/i);
@@ -70,9 +106,9 @@ describe('App', () => {
     const sortButton = screen.getByText(/Sort by episode (descending)/i);
     sortButton.click();
 
-    const listItems = screen.getAllByRole('listitem');
-    const firstMovie = listItems[0];
-    const lastMovie = listItems.at(-1);
+    const movies = screen.getAllByRole('button');
+    const firstMovie = movies[0];
+    const lastMovie = movies.at(-1);
 
     expect(firstMovie).toHaveTextContent(/The Phantom Menace/i);
     expect(lastMovie).toHaveTextContent(/Return of the Jedi/i);
@@ -85,7 +121,7 @@ describe('App', () => {
     const searchField = screen.getByPlaceholderText(/Type to search/i);
     user.type(searchField, 'Empire');
 
-    expect(screen.getAllByRole('listitem')).toHaveLength(1);
-    expect(screen.getAllByRole('listitem')[0]).toHaveTextContent(/The Empire Strikes Back/i);
+    expect(screen.getAllByRole('button')).toHaveLength(1);
+    expect(screen.getAllByRole('button')[0]).toHaveTextContent(/The Empire Strikes Back/i);
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
+import { Grid, List, ListItemButton, ListItemText } from '@mui/material';
+import { useState } from 'react';
 import useFetchMovies from './hooks/useFetchMovies';
 
 const App = () => {
+  const [selectedIndex, setSelectedIndex] = useState<number | undefined>();
   const { data: movies } = useFetchMovies();
 
+  const handleListItemClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>, index: number) => {
+    setSelectedIndex(index);
+  };
+
   return (
-    <>
-      {movies?.results?.map(movie => (
-        <li key={movie.episode_id}>{movie.title}</li>
-      ))}
-    </>
+    <Grid container width='100vw'>
+      <Grid item xs={6}>
+        <List>
+          {movies?.results?.map(movie => {
+            const { title, episode_id } = movie;
+            return (
+              <ListItemButton
+                key={episode_id}
+                selected={selectedIndex === episode_id}
+                onClick={event => handleListItemClick(event, episode_id)}
+              >
+                <ListItemText primary={`EPISODE ${episode_id} - ${title}`} />
+              </ListItemButton>
+            );
+          })}
+        </List>
+      </Grid>
+      <Grid item xs={6}>
+        {!selectedIndex ? (
+          <h1>Select a movie to view details</h1>
+        ) : (
+          <>
+            <h1>{movies?.results?.find(movie => movie.episode_id === selectedIndex)?.title}</h1>
+            <p>{movies?.results?.find(movie => movie.episode_id === selectedIndex)?.opening_crawl}</p>
+          </>
+        )}
+      </Grid>
+    </Grid>
   );
 };
 
